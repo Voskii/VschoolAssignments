8bf23f1e31e67355a4a1954c9b1a0b7bbdfeb333
Kyle Shutt <xavior04350@yahoo.com>
2022-04-13 20:16:55 -0400

    map method exercise update

--- level2/Map Exercises/map.js
+++ level2/Map Exercises/map.js
@@ -22,14 +22,83 @@
 // }
 // stringItUp(myArr);
 // 3
-let myArr = ["john", "JACOB", "jinGleHeimer", "schmidt"];
-function capitalizeNames(arr){
-let unCap = arr.map(function(fish){
-    fish.toLowerCase() + fish[0].toUpperCase();
-})
+// let myArr = ["john", "JACOB", "jinGleHeimer", "schmidt"];
+// function capitalizeNames(arr){
+//     let kekW = arr.map(function(fish){
+//     let lulW = fish.toLowerCase();
+//     let wow = lulW.slice([1]);
+//     let upF = fish[0].toUpperCase();
+//     let result = upF.concat(wow);
+//         return result
+// })
+// console.log(kekW);
 
-console.log(unCap);
-return unCap;
-}
-capitalizeNames(myArr);
+// }
+// capitalizeNames(myArr);
+
+//4
+
+function namesOnly(arr){
+    arr.map(function(fish){
+    let nnArr = [];
+        console.log(nnArr);
+        return nnArr.push(fish.name);
+    })
+  }
+  
+  console.log(namesOnly([
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ]));
+
+// for(let f = 0; f < fish[1]; f++){
+//             console.log("I am in the for loop");
+//             fish[0].toUpperCase();
+//             console.log(fish[0].toUpperCase());
+//         }
+//     })
+// let lulW = kekW.map(function(taco){
+// let chick = taco[0].toUpperCase()
+//     return taco.splice(taco[0], taco[1], chick);
+// })
+// console.log(lulW);
+// fish.toLowerCase().fish.map((chips) => {chips.toUpperCase()}))return kekW;};
+// splice(upMe[0], upMe[1], upMe[0].toUpperCase()
 // prototype.toUpperCase();
+//  taco.splice(fish[0], fish[1]);
+// function capitalizeNames(arr){
+
+//     let kekW = arr.map(function(fish){
+//     let newArr = []
+//         newArr.push(fish.toLowerCase());
+//         console.log(newArr);
+//     let chips = newArr.map(function(upMe){
+//     let newerArr = []
+//         newerArr.push(upMe[0].toUpperCase());
+//         console.log(newerArr);
+//         })
+        
+//         return chips;
+//     })
+    
+//     return kekW;
+    
+//     }
