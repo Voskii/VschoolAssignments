7cd292aa6258bd421216e5232b587f4244403aa3
Kyle Shutt <xavior04350@yahoo.com>
2022-04-18 13:30:47 -0400

    updates old and new

--- String Methods Exercise/sme.js
+++ String Methods Exercise/sme.js
@@ -1,61 +1,61 @@
-// capilizeAndLowercase("HelLo") // => "HELLOhello"
-// function capilizeAndLowercase(){
-// var thisW = "HelLo";
-// var thisWW = thisW.toUpperCase();
-// var thisWWW = thisWW.toLowerCase();
-// console.log(thisWW, thisWWW);
-// }
-// capilizeAndLowercase();
+capilizeAndLowercase("HelLo") // => "HELLOhello"
+function capilizeAndLowercase(){
+var thisW = "HelLo";
+var thisWW = thisW.toUpperCase();
+var thisWWW = thisWW.toLowerCase();
+console.log(thisWW, thisWWW);
+}
+capilizeAndLowercase();
 
-// findMiddleIndex("Hello") // => 2
-// findMiddleIndex("Hello World") // => 5
-// let hey = "Hello"
-// function findMiddleIndex(high){
-// let fish = Math.floor(high.length / 2);
-// console.log(fish);
-// }
-// findMiddleIndex(hey);
+findMiddleIndex("Hello") // => 2
+findMiddleIndex("Hello World") // => 5
+let hey = "Hello"
+function findMiddleIndex(high){
+let fish = Math.floor(high.length / 2);
+console.log(fish);
+}
+findMiddleIndex(hey);
 
-// returnFirstHalf("Hello") // => "He"                                  // did this one twice but different ways
-// returnFirstHalf("Hello World") // => "Hello"
-// hey = "Hello"
-// heyW = "Hello World"
-// function returnFirstHalf(high,highW){
-// var fish = high.substring(0, high.length / 2)
-// console.log(fish);
-// var cheese = highW.substring(0, highW.length / 2)                
-// console.log(cheese);
-// }
-// returnFirstHalf(hey,heyW);
+returnFirstHalf("Hello") // => "He"                                  // did this one twice but different ways
+returnFirstHalf("Hello World") // => "Hello"
+hey = "Hello"
+heyW = "Hello World"
+function returnFirstHalf(high,highW){
+var fish = high.substring(0, high.length / 2)
+console.log(fish);
+var cheese = highW.substring(0, highW.length / 2)                
+console.log(cheese);
+}
+returnFirstHalf(hey,heyW);
 
-// returnFirstHalf("Hello") // => "He"
-// returnFirstHalf("Hello World") // => "Hello"
-// var hey = "Hello"
-// var heyW = "Hello World"
-// function returnFirstHalf(high,highW){
-// var fish = high.slice(0, high.length / 2)
-//     console.log(fish);
-// var cheese = highW.slice(0, highW.length / 2)
-//     console.log(cheese);
-// }
-// returnFirstHalf(hey,heyW);
+returnFirstHalf("Hello") // => "He"
+returnFirstHalf("Hello World") // => "Hello"
+var hey = "Hello"
+var heyW = "Hello World"
+function returnFirstHalf(high,highW){
+var fish = high.slice(0, high.length / 2)
+    console.log(fish);
+var cheese = highW.slice(0, highW.length / 2)
+    console.log(cheese);
+}
+returnFirstHalf(hey,heyW);
 
-// capitlizeAndLowercase("Hello") // => "HEllo"
-// capitalizeAndLowercase("Hello World") // => "HELLO world"
-// var hey = "Hello"
-// var heyW = "Hello World"
-// function capitalizeAndLowercase(high, highW){
-// var fish = high.slice(0, high.length / 2)
-// var bbq = high.slice(high.length / 2)
-// var fishS = fish.toUpperCase();
-// var fishSb = fishS.concat(bbq);
-// console.log(fishSb);
-// var yaoo = Math.floor(highW.length / 2)
-// var fhCap = highW.slice(0, yaoo);
-// var shLow = highW.slice(yaoo);
-// var fhCapD = fhCap.toUpperCase();
-// var shLowD = shLow.toLowerCase();
-// var newWor = fhCapD.concat(shLowD);
-// console.log(newWor)
-// }
-// capitalizeAndLowercase(hey,heyW);
+capitlizeAndLowercase("Hello") // => "HEllo"
+capitalizeAndLowercase("Hello World") // => "HELLO world"
+var hey = "Hello"
+var heyW = "Hello World"
+function capitalizeAndLowercase(high, highW){
+var fish = high.slice(0, high.length / 2)
+var bbq = high.slice(high.length / 2)
+var fishS = fish.toUpperCase();
+var fishSb = fishS.concat(bbq);
+console.log(fishSb);
+var yaoo = Math.floor(highW.length / 2)
+var fhCap = highW.slice(0, yaoo);
+var shLow = highW.slice(yaoo);
+var fhCapD = fhCap.toUpperCase();
+var shLowD = shLow.toLowerCase();
+var newWor = fhCapD.concat(shLowD);
+console.log(newWor)
+}
+capitalizeAndLowercase(hey,heyW);
--- level2/Map Exercises/map.js
+++ level2/Map Exercises/map.js
@@ -12,6 +12,7 @@
 // //  // [4, 10, 200]
 
 // 2
+
 // let myArr = [2, 5, 100];
 
 // function stringItUp(arr){
@@ -21,6 +22,8 @@
 // console.log(taco);
 // }
 // stringItUp(myArr);
+
+
 // 3
 // let myArr = ["john", "JACOB", "jinGleHeimer", "schmidt"];
 // function capitalizeNames(arr){
@@ -38,36 +41,143 @@
 
 //4
 
-function namesOnly(arr){
-    arr.map(function(fish){
-    let nnArr = [];
-        console.log(nnArr);
-        return nnArr.push(fish.name);
-    })
-  }
-  
-  console.log(namesOnly([
-      {
-          name: "Angelina Jolie",
-          age: 80
-      },
-      {
-          name: "Eric Jones",
-          age: 2
-      },
-      {
-          name: "Paris Hilton",
-          age: 5
-      },
-      {
-          name: "Kayne West",
-          age: 16
-      },
-      {
-          name: "Bob Ziroll",
-          age: 100
-      }
-  ]));
+// arrayOfNames = [
+// {
+// name: "Angelina Jolie",
+// age: 80
+// },
+// {
+// name: "Eric Jones",
+// age: 2
+// },
+// {
+// name: "Paris Hilton",
+// age: 5
+// },
+// {
+// name: "Kayne West",
+// age: 16
+// },
+// {
+// name: "Bob Ziroll",
+// age: 100
+// }
+// ];
+// function namesOnly(arr){
+
+//     let music = arr.map(function(fish){
+//         let nnArr = [];
+//             return nnArr.concat(fish.name);
+//         })
+//     console.log(music)
+//     }
+//     namesOnly(arrayOfNames);
+
+//5
+
+// arrayOfNames = [
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+//     {
+//         name: "Bob Ziroll",
+//         age: 100
+//     }
+// ];
+
+// function makeStrings(arr){
+// let okay = arr.map(function(fish){
+//     let heyy = [];
+//     if(fish.age >= 18){
+//         return heyy.concat(fish.name + ' can go')
+//     }
+//     else {
+//         return heyy.concat(fish.name + ' cannot go')
+//     }
+// })
+//     console.log(okay)
+// }
+// makeStrings(arrayOfNames);
+
+// 6
+
+function readyToPutInTheDOM(arr){
+let whatName = arr.map(function(fish){
+    let hey = [];
+    return hey.concat("<h1>" + fish.name + "</h1>")
+})
+let whatAge = arr.map(function(fish){
+    let heyy = [];
+    return heyy.concat("<h2>" + fish.age + "</h2>")
+})
+console.log(whatName.concat(whatAge))
+}
+// consider split slice splice
+let peopleArr = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+];
+readyToPutInTheDOM(peopleArr);
+  // ["<h1>Angelina Jolie</h1><h2>80</h2>",
+  // "<h1>Eric Jones</h1><h2>2</h2>",
+  // "<h1>Paris Hilton</h1><h2>5</h2>",
+  // "<h1>Kayne West</h1><h2>16</h2>",
+  // "<h1>Bob Ziroll</h1><h2>100</h2>"]
+
+// function makeStrings(arr){
+// let smiles = arr.map(function(fish){
+// let actorAge = fish.age;
+// let actorName = fish.name;
+//     return actorAge + actorName;
+// }) 
+// console.log(smiles);
+//     if(smiles.age >= 18){
+//         let sure = [" can go"];
+//         sure.concat(smiles.name);
+//         console.log(sure);
+// }
+// }
+// function namesOnly(arr){
+
+// let music = arr.map(function(fish){
+//     let nnArr = [];
+//         return nnArr.concat(fish.name);
+//     })
+// console.log(music)
+// }
+// namesOnly(arrayOfNames);
+
 
 // for(let f = 0; f < fish[1]; f++){
 //             console.log("I am in the for loop");
