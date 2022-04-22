c179dbe7168b4c50ab035d8edfa2c9a815055f1e
Kyle Shutt <xavior04350@yahoo.com>
2022-04-21 20:03:50 -0400

    Catch Me If You Can Assignment

---
+++ level2/Catch Me If You Can/cmiyc.js
@@ -0,0 +1,34 @@
+// 1a
+// const val1 = 4
+// const val2 = 'this is not a number bro'; 
+// function sum(x, y){
+//     try {
+//         if(x !== Number(val1) || y !== Number(val2)){
+//             throw "Hey that is not a NUMBER"
+//         }
+//         console.log('NUMBERS!!!')
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+//     // console.log(x + y)
+//     return x + y;
+// }
+// sum(val1, val2);
+//1b
+// const val1 = 4
+// const val2 = 'this is not a number bro'; 
+// function sum(x, y){
+//     try {
+//         if(x !== Number(val1) || y !== Number(val2)){
+//             throw "Hey that is not a NUMBER"
+//         }
+//         console.log('NUMBERS!!!')
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+//     // console.log(x + y)
+//     return x + y;
+// }
+// sum('1', '2');
