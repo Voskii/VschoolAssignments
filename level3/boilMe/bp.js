30391754c85cdc534b5f1bb1c8609e4cb0ffe4c0
Kyle Shutt <xavior04350@yahoo.com>
2022-05-30 13:59:23 -0400

    practice template

---
+++ level3/boilMe/bp.js
@@ -0,0 +1,3 @@
+// ReactDom.render(<h1>SMADDUH THATTTTTTTTTTTTTTT</h1>, document.getElementById("root"))
+
+// ReactDom.Render(<ul>FISH<li>trout</li><li>salmon</li></ul>, dpcument.getElementById('root'))
