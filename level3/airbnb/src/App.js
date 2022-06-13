3461d3da1e3f8d37a33d86577b7ede9dafa7a5b9
Kyle Shutt <xavior04350@yahoo.com>
2022-06-13 15:20:23 -0400

    proj 2 start

---
+++ level3/airbnb/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <Card />
+        </div>
+    )
+}
---
+++ level3/airbnb/src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero() {
+    return (
+        <section className="hero">
+                <img src="./images/group.png" className="heroimg" alt=""/>
+                <h1 className="heroheader">Online Experiences</h1>
+                <p className="herotext">Join the unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+}
