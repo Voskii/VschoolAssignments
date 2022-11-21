4b7fd163394fa2ae6ca551a6461afe31be2169f7
Kyle Shutt <xavior04350@yahoo.com>
2022-11-21 16:29:03 -0500

    speedclicker practice

--- level4/speedclicker/src/App.js
+++ level4/speedclicker/src/App.js
@@ -1,11 +1,33 @@
 // I am going to write this program until I am efficient and understand it's concepts 
+import React, {useState} from "react";
 
-import './App.css';
 
 export default function App() {
+  const [text, setText] = useState('')
+
+  function handleChange(e){
+    const {value} = e.target
+    setText(value)
+  }
+  console.log(text)
+
+  function countWords(text){
+    const fish = text.split(' ')
+    
+    const filterMe = fish.filter(word => word !== '')
+    return filterMe.length
+  }
+
   return (
     <div className="App">
-      
+      <h1>How Fast do you type?</h1>
+      <textarea 
+        onChange={handleChange}
+        value={text} 
+      />
+      <h4>Display the amount of time remaining</h4>
+      <button onClick={() => console.log(countWords(text))}>Start</button>
+      <h1>word count</h1>
     </div>
   );
 }
