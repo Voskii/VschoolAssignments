4004115a44b28f0df5ebb1598215a6dc4a9cfd1c
Kyle Shutt <xavior04350@yahoo.com>
2022-12-07 17:08:39 -0500

    uglythings prog

--- level4/uglythings/src/App.js
+++ level4/uglythings/src/App.js
@@ -1,7 +1,7 @@
 // LET'S GO BROTHA!!
 import React, { useState, useEffect, useContext, createContext } from "react"
 import axios from "axios";
-// import Beauty from "./Beauty";
+import Beauty from "./Beauty";
 
 export default function App() {
 
@@ -16,39 +16,40 @@ export default function App() {
     )
   }
 
-  const [allThings, setAllThings] = useState('')
-  console.log(allThings)
+  const [allThings, setAllThings] = useState({
+    title: "",
+    imgURL: "",
+    description: ""
+  })
   
+  const [userThing, setUserThing] = useState({
+    title: "",
+    imgURL: "",
+    description: ""
+  })
   
-  function handleSubmit(){
+  // this needs work obv lol
+  function handleSubmit(e){
     console.log('Mama had a baby, issa boy KEK')
     e.preventDefault()
-         //meme.topText + meme.bottomText + meme.randomImage + `,`
-        setSaveMeme(prevSaveMeme => [...prevSaveMeme, meme])
-        setMeme({topText: "",
-        bottomText: "",
-        randomImage: meme.randomImage
-        })
-        console.log(saveMeme + `yo`)
+    // take userThing state and post to api     
+        // setAllThings(prevAllThings => [...prevAllThings, meme])
+        
   }
-  
   
   function handleChange(event){
     console.log(`Give me fuel, give me fire, give me that which I desire.. HUAHHH`)
     const {name, value} = event.target
-        setAllThings(prevThing => ({
+        setUserThing(prevThing => ({
             ...prevThing,
             [name]: value
         }))
-  }
-
-  function Beauty(){
-    //useContext here
-    
+    console.log(userThing)
   }
 
   useEffect(() => {
     console.log("useffect triggered")
+    
     axios.get('https://api.vschool.io/kyleshutt/thing')
     
     .then(res => {console.log(res.data) 
@@ -57,20 +58,43 @@ export default function App() {
     
   }, []);
 
+  //look up map syntax, don't have to render component 
+  const beauty = allThings.map((thing, index) => (<Beauty key={index} data={thing}/>))
+
   return (
     <div className="App">
       <form className='formm' onSubmit={handleSubmit}>
         <div className='container'>
-          <input className='inputMe' name="fName" min="3" value={allThings} placeholder="Title" type="text" onChange={handleChange}/>
-          <input className='inputMe' name="lName" value={allThings} placeholder="Img URL" type="text" onChange={handleChange}/>
-          <input className='desc inputMe' name="desc" value={allThings} placeholder="Why do you think this is Beautiful" type="text" onChange={handleChange}/>
+          <input 
+            className='inputMe'
+            placeholder="Title"
+            name="title"  
+            value={userThing.title} 
+            type="text" 
+            onChange={handleChange}
+          />
+          <input className='inputMe' 
+            name="imgURL" 
+            value={userThing.imgURL} 
+            placeholder="Img URL" 
+            type="text" 
+            onChange={handleChange}
+          />
+          <input 
+            className='desc inputMe' 
+            name="description" 
+            value={userThing.description} 
+            placeholder="Why do you think this is Beautiful" 
+            type="text" 
+            onChange={handleChange}
+          />
         </div>
         <div className='bottom2'>
           <button>UwU</button>
         </div>
       </form>
       <div>
-        {Beauty}
+        {beauty}
       </div>
     </div>
   );
--- level4/uglythings/src/Beauty.js
+++ level4/uglythings/src/Beauty.js
@@ -1,10 +1,92 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios";
 
-export default function Beauty(){
-    return (
-        <div>
+export default function Beauty(props){
+
+    const {fish, data, id, saveBeauty, setSaveBeauty} = props
+    
+    const [visible, setVisible] = useState(!true)
+    
+    const [beautyInputs, setBeautyInputs] = useState({
+        topText: props.data.topText,
+        bottomText: props.data.bottomText
+    })
+
+    
+// donno if this fully worky lol id a lil sus
+    const updateBeautyItem = (id, updatedItem) => {
+        
+        console.log(`updateBeautyItem function has been called`,id)
+        props.setSaveBeauty(prevsaveBeauty => {
+           return prevsaveBeauty.map((item, index)=> {
+                if (index === id) {
+                    return updatedItem
+                }
+                else {
+                    return item
+                }
+             
+            })
             
+        })
+        
+    }
+
+    const editMe = () => {
+        setVisible((prev) => !prev);
+    }
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setBeautyInputs(prevState=>{
+            return ({
+                ...prevState,
+                [name]:value 
+            })
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('form has been submitted')
+        updateBeautyItem(props.id, beautyInputs)
+    }
+
+    const deleteMe = (index) => {
+        console.log(props)
+        console.log(typeof(props))
+        setSaveBeauty(preSaveBeauty => {
+            return [...preSaveBeauty.filter(item => item !== preSaveBeauty[index])]
+        }
+    )}
+
+    return (
+        <div >
+                <div>
+                    {visible ?
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                name="topText"
+                                value={beautyInputs.topText}
+                                onChange={handleChange}
+                            />
+                            <input
+                                type="text"
+                                name="bottomText"
+                                value={beautyInputs.bottomText}
+                                onChange={handleChange}
+                            />
+                        </form>
+                    :
+                    <div className="container">
+                    <img src={props.data.randomImage} className="bBeauty--image" alt=""/>
+                    <h2 className="bBeauty--text top">{beautyInputs.topText}</h2>
+                    <h2 className="bBeauty--text bottom">{beautyInputs.bottomText}</h2>
+                    </div>
+                    }
+                    <button id="editButt" onClick={editMe}>{visible ? "Close" : 'Edit'}</button>
+                    <button onClick={() => deleteMe(fish)}>DELETE</button>
+                </div>
         </div>
     )
 }
