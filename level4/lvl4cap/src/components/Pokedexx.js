e1c109bb95d1c474ff6b56d23a12f6c4eb873d7e
Kyle Shutt <xavior04350@yahoo.com>
2023-02-28 17:53:19 -0600

    lvl4 cap updates

--- level4/lvl4cap/src/components/Pokedexx.js
+++ level4/lvl4cap/src/components/Pokedexx.js
@@ -5,10 +5,16 @@ import Carousel from "./Carousel";
 import Switch from "react-switch";
 import poke from '../images/pokeball.png'
 import pika from '../images/pikachu.png'
+import opika from '../images/oface.png'
+import ashe from '../images/ashepchu.png'
+import axios from "axios";
 
 export default function Pokedex() {
     
     var [date,setDate] = useState(new Date());
+    const [whoDat, setWhoDat] = useState(false)
+    const [userPoke, setUserPoke] = useState({})
+    const [pokeName, setPokeName] = useState('')
     
     useEffect(() => {
         var timer = setInterval(()=>setDate(new Date()), 1000 )
@@ -17,27 +23,100 @@ export default function Pokedex() {
         }
     
     });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setPokeName({[name]:value})
+        console.log(`handleCHANGE FUNC:`, pokeName)
+    }
+    
+
+    const pokeReveal = () => {
+        
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName.name}`)
+        .then(res => setUserPoke({
+                id: res.data.id,
+                name: res.data.species.name.charAt(0).toUpperCase() + res.data.species.name.slice(1),
+                hp: res.data.stats[0].base_stat,
+                attack: res.data.stats[1].base_stat,
+                def: res.data.stats[2].base_stat,
+                speed: res.data.stats[5].base_stat,
+                img: res.data.sprites.other.home.front_default,
+                imgB: res.data.sprites.front_default,
+                types: res.data.types,
+                attacks: res.data.abilities,
+                species: res.data.species.url,
+                height: res.data.height,
+                weight: res.data.weight
+        }))
+        .catch(error => console.log(error))
+        
+        
+        console.log(`inside poke Reveal:`, pokeName, userPoke)
+        setWhoDat(!whoDat)
+        setPokeName({name: ''})
+        
+    }
+
+    const flavor = () => {
+        axios.get(userPoke.species)
+        .then(res => console.log(`flavor text grab:`, res.data))
+        .catch(error => console.log(error))
+    }
+    
     
     return (
         <div className="pd-grid-container">
-            <div className="box1">
-                <div className="time">
-                    <div>{date.toLocaleDateString()}</div>
-                    <div>{date.toLocaleTimeString()}</div>
+                {whoDat
+                ?
+                <div className="box1"> 
+                    <div className="time">
+                        <div>{date.toLocaleDateString()}</div>
+                        <div>{date.toLocaleTimeString()}</div>
+                    </div>
+                    <img src={userPoke.img
+                        ?
+                        userPoke.img
+                        :
+                        userPoke.imgB}  
+                        className={userPoke.img?'who-dat-img':'who-dat-imgg'} alt='' /> 
+                    <div className="info-container">
+                        <h6 className="stat1">Spd:{userPoke.speed}</h6>
+                        <h6 className="stat2">Hp:{userPoke.hp}</h6>
+                        <h6 className="stat3">Atk:{userPoke.attack}</h6>
+                        <h6 className="stat4">Height: {userPoke.height}</h6>
+                        <h6 className="stat5">Weight: {userPoke.weight}</h6>
+                    </div>
                 </div>
-                <input style={{height: '200px', width: '220px', alignSelf:"center", fontFamily: "Pokemon GB"}} placeholder='...Search'></input>
-                <div className="info-container">
-                    <h6 className="stat1">Spd:56</h6>
-                    <h6 className="stat2">Hp:56</h6>
-                    <h6 className="stat3">Atk:56</h6>
-                    <h6 className="stat4">Height: 56cm</h6>
-                    <h6 className="stat5">Weight: 56kg</h6>
+                :
+                    <div className="box1">
+                        <div className="time">
+                        <div>{date.toLocaleDateString()}</div>
+                        <div>{date.toLocaleTimeString()}</div>
+                    </div>
+                    <input 
+                    className="who-dat-input" 
+                    placeholder='...Search' 
+                    onChange={handleChange}
+                    value={pokeName.name}
+                    name="name">
+                    </input>
+                    <div className="info-container">
+                        <h6 className="stat1">Spd: </h6>
+                        <h6 className="stat2">Hp: </h6>
+                        <h6 className="stat3">Atk: </h6>
+                        <h6 className="stat4">Height: </h6>
+                        <h6 className="stat5">Weight: </h6>
+                    </div>
                 </div>
-            </div>
+                    
+                    
+                }
+            
             <div className="box2">
                 <div className="batt-sub">
                     <div style={{border: 'solid 2px aqua'}}>BATT %</div>
-                    <button className="sub-butt">Find</button>
+                    <button className="sub-butt" onClick={() => pokeReveal()}>{whoDat? 'Find Poke' : 'Who Dat?'}</button>
                 </div>
                 <div className="keypad-container ">
                     <div className="key"></div>
@@ -49,8 +128,15 @@ export default function Pokedex() {
                     <div className="key"></div>
                     <div className="key"></div>
                 </div>
-                <input style={{height: '50px', width: '220px', alignSelf:"center", fontFamily: "Pokemon GB"}} placeholder='Description'></input>
+                {whoDat
+                ?
+                    <input style={{height: '50px', width: '220px', alignSelf:"center", fontFamily: "Pokemon GB"}} value={userPoke.species}></input>
+                :
+                    <input style={{height: '50px', width: '220px', alignSelf:"center", fontFamily: "Pokemon GB"}} value='...description...'></input>
+                }
             </div>
+            {whoDat
+            ?
             <div className="box3">
                 <div>
                     <div className="evo-container">
@@ -68,15 +154,26 @@ export default function Pokedex() {
                         </div>
                     </div>
                     <div>
-                        Types
+                        Types map
                     </div>
                 </div>
             </div>
+            :
+            ''
+            }
+            
             <div className="box4">
-                <div>
-                    
-                    <img src={pika} style={{height: "60%", width: '80%'}}/>
-                </div>
+            {whoDat
+            ?
+            <div>
+                <img src={pika} style={{height: "60%", width: '80%'}}/>
+            </div>
+            :
+            <div>
+                <img src={opika} style={{height: "60%", width: '80%'}}/>
+            </div>
+            }
+                
             </div>
         </div>
     )
