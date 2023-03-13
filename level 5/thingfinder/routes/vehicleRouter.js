016227a356f86a9620d7b68340db2df2272c7263
Kyle Shutt <xavior04350@yahoo.com>
2023-03-13 13:18:34 -0500

    thingfinder

--- level 5/thingfinder/routes/vehicleRouter.js
+++ level 5/thingfinder/routes/vehicleRouter.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require('uuid')
 const vehicles = [
     {name: "f-150 raptor", type: "truck", price: 32000, _id: uuidv4()},
     {name: "challenger r/t", type: "car",price: 50000, _id: uuidv4()},
-    {name: "GT 500", type: "car", price: 80795, _id: uuidv4()},
+    {name: "gt 500", type: "car", price: 80795, _id: uuidv4()},
     {name: "ford gt", type: "car", price: 500000, _id: uuidv4()},
     {name: "ram trx", type: "truck", price: 84355, _id: uuidv4()},
     {name: "hummer", type: "truck", price: 105595, _id: uuidv4()},
@@ -42,11 +42,12 @@ vehicleRouter.get('/search/name', (req, res) => {
 })
 //get by price
 
-vehicleRouter.get('./search/price', (req, res) => {
+vehicleRouter.get('/search/price', (req, res) => {
     console.log(req)
     const price = req.query.price
     const filteredPrice = vehicles.filter(vehicle => vehicle.price === price)
-    res.send(filteredPrice)
+    res.send(`${filteredPrice}`)
+    //this does not work
 })
 
     module.exports = vehicleRouter
